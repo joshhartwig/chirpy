2344175861c55952f954d65514e48dcbfe93d1ed
Josh Hartwig <joshman211@gmail.com>
2024-10-07 16:41:16 -0500

    added jwt creation

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,10 +2,14 @@ package auth
 
 import (
 	"errors"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// HashPassword hashes a plain text password using bcrypt.
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
@@ -14,7 +18,29 @@ func HashPassword(password string) (string, error) {
 	return string(bytes), nil
 }
 
+// CheckPasswordHash compares a plain text password with a hashed password.
 func CheckPasswordHash(password, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
+}
+
+// MakeJWT generates a JWT token for a given user ID with a specified expiration time.
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	if tokenSecret == "" {
+		return "", errors.New("token secret not set")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userId.String(),
+	})
+
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", errors.New("unable to sign string")
+	}
+
+	return tokenString, nil
 }
